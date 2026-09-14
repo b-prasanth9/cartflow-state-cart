381f3bef084c1c087aa16d11e08a44fc5b61fd4f
b-prasanth9 <r02737758@gmail.com>
2026-09-14 10:04:02 +0530

    Add CartFlow repository

---
+++ src/main/java/com/prasanth/cartflow/repository/CartItemRepository.java
@@ -0,0 +1,3 @@
+package com.prasanth.cartflow.repository;
+import com.prasanth.cartflow.model.CartItem; import org.springframework.data.jpa.repository.JpaRepository; import java.util.List;
+public interface CartItemRepository extends JpaRepository<CartItem,Long>{ List<CartItem> findByCartId(String cartId); }
